96ac2efabcd043323bd118d286438012ffb8ab12
Umesh Ghimire <me.umeshuser@gmail.com>
2023-04-19 10:13:53 +0545

    Update instawp.php

--- instawp.php
+++ instawp.php
@@ -3,8 +3,8 @@
  * Plugin Name:   InstaWP
  * Plugin URI:    https://wpeverest.com
  * Description:   InstaWP Git Deployment testing
- * Version:       1.0.4
- * Author:        Vikas
+ * Version:       1.0.6
+ * Author:        Umesh Ghimire
  * Author URI:    https://wpeverest.com
  * Text Domain:   instawp
  * Domain Path:   /languages
