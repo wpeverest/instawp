52a751b1eacc4bf24848f5445b0cb32ec854c146
Umesh Ghimire <me.umeshuser@gmail.com>
2023-04-19 09:33:05 +0545

    init

---
+++ instawp.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:   InstaWP
+ * Plugin URI:    https://wpeverest.com
+ * Description:   InstaWP Git Deployment testing
+ * Version:       1.0.0
+ * Author:        Vikas
+ * Author URI:    https://wpeverest.com
+ * Text Domain:   instawp
+ * Domain Path:   /languages
+ */
+
+// Exit if accessed directly.
+if (!defined('ABSPATH')) exit;
+
+// Include your custom code here.
